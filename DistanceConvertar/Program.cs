e8a3fb760847073c9d00d1f7b4f3bc4531c3223c
加藤 信之介 <32057@ojs.ac.jp>
2021-05-21 15:53:11 +0900

    P49 リスト2.4

--- DistanceConvertar/Program.cs
+++ DistanceConvertar/Program.cs
@@ -9,19 +9,28 @@ namespace DistanceConvertar {
         static void Main(string[] args) {
             if (args.Length >= 1 && args[0] == "-tom") {
                 //フォール度からメートルへの対応表を出力
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
-                }
+                PrintFeetToMeterList(1,10);
             } else {
                 //フォール度からメートルへの対応表を出力
-                for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0} m = {1:0.0000} fm", meter, feet);
-
-                }
+                PrintMeterToFeetList(1,10);
             }
         }
+        //フォール度からメートルへの対応表を出力
+        private static void PrintMeterToFeetList(int start,int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} m = {1:0.0000} fm", meter, feet);
+
+            }
+        }
+        //フォール度からメートルへの対応表を出力
+        private static void PrintFeetToMeterList(int start, int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+
         //フィートからメートルを求める
         static double FeetToMeter(int feet) {
                 return feet * 0.3048;           
