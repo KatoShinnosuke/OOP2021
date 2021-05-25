dee82caa133f0f8b58ebfff3452260910f9b477a
加藤 信之介 <32057@ojs.ac.jp>
2021-05-25 11:34:35 +0900

    P58リスト2-14 売上クラス作成

---
+++ SalesCalculator/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    //売上クラス
+    class Sale {
+        //店舗名
+        public String ShopName { get; set; }
+        //商品カテゴリー
+        public String ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+    }
+}
