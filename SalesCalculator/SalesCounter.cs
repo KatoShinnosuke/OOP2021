6c76c1b88d0deea979beb2f310cc70a70bcf9b1a
加藤 信之介 <32057@ojs.ac.jp>
2021-05-27 15:50:53 +0900

    P70リスト2-22

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +11,34 @@ namespace SalesCalculator {
         private List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
         public Dictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
-                if (dict.ContainsKey(sale.ShopName)) 
+                if (dict.ContainsKey(sale.ShopName))
                     //すでにコレクションに店舗が設定されてる
                     dict[sale.ShopName] += sale.Amount;
-                 else
+                else
                     //コレクション店舗を登録
                     dict[sale.ShopName] = sale.Amount;
-                }
-                return dict;
             }
+            return dict;
+        }
+       private static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
     }
+}
