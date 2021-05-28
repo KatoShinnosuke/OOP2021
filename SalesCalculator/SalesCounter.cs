d7fa20c76ab92f25de6243e1a33d4ce7adc87c0f
加藤 信之介 <32057@ojs.ac.jp>
2021-05-28 14:05:45 +0900

    P77 リスト2-27　完成版

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -8,13 +8,13 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     //売り上げを集計するクラス
     class SalesCounter {
-        private List<Sale> _sales;
+        private IEnumerable<Sale> _sales;
 
         //コンストラクタ
         public SalesCounter(string filePath) {
             _sales = ReadSales(filePath);
         }
-        public Dictionary<string, int> GetPerStoreSales() {
+        public IDictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
@@ -26,7 +26,7 @@ namespace SalesCalculator {
             }
             return dict;
         }
-       private static List<Sale> ReadSales(string filePath) {
+       private static IEnumerable<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
