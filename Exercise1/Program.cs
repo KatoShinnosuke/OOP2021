0854dbf31846f65a5366d4a493003c569be968d1
加藤 信之介 <32057@ojs.ac.jp>
2021-05-28 15:38:03 +0900

    第2章演習問題　2-1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            Song[] songs = new Song[2];
+
+            songs[0] = new Song("僕のこと", "ミセス", 310);
+            songs[1] = new Song("青と夏", "ミセス", 310);
+            
+            foreach(var i in songs) {
+                Console.WriteLine("曲名:{0},歌手:{1},演奏時間:{2}:{3:00}",i.Title1,i.ArtistName1,(i.Length1/60),(i.Length1%60));
+            }
+        }
+    }
+}
---
+++ Exercise1/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Song {
+        private string Title;
+        private string ArtistName;
+        private int Length;
+
+        public string Title1 { get => Title; set => Title = value; }
+        public string ArtistName1 { get => ArtistName; set => ArtistName = value; }
+        public int Length1 { get => Length; set => Length = value; }
+        public Song(string Title,string ArtistName,int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+    }
+}
