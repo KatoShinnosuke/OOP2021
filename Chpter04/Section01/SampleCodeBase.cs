d63b5f930f72a17f67ec90b1ebc055a2c9517184
加藤 信之介 <32057@ojs.ac.jp>
2021-06-08 09:42:31 +0900

    Chapter4

---
+++ Chpter04/Section01/SampleCodeBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Section01 {
+    internal class SampleCodeBase {
+        public int Count(int num) {
+            var number = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            int count = 0;
+            foreach (var n in number) {
+                if (n == num) {
+                    count++;
+                }
+                return count;
+            }
+        }
+    }
+}
