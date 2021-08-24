25e3dfdd066a5f36f7dab26cf7476b9c6f4d8184
信之介 加藤 <32057@ojs.ac.jp>
2021-08-24 20:25:13 +0900

    20210823べき乗

---
+++ Sampke0823/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Sampke0823
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exec = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Value = new System.Windows.Forms.TextBox();
+            this.Jyou = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // exec
+            // 
+            this.exec.Location = new System.Drawing.Point(447, 151);
+            this.exec.Name = "exec";
+            this.exec.Size = new System.Drawing.Size(53, 38);
+            this.exec.TabIndex = 0;
+            this.exec.Text = "結果→";
+            this.exec.UseVisualStyleBackColor = true;
+            this.exec.Click += new System.EventHandler(this.exec_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "の";
+            // 
+            // Value
+            // 
+            this.Value.Location = new System.Drawing.Point(12, 161);
+            this.Value.Name = "Value";
+            this.Value.Size = new System.Drawing.Size(100, 19);
+            this.Value.TabIndex = 2;
+            // 
+            // Jyou
+            // 
+            this.Jyou.Location = new System.Drawing.Point(234, 165);
+            this.Jyou.Name = "Jyou";
+            this.Jyou.Size = new System.Drawing.Size(100, 19);
+            this.Jyou.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "乗";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(546, 172);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(100, 19);
+            this.Result.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Jyou);
+            this.Controls.Add(this.Value);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Value;
+        private System.Windows.Forms.TextBox Jyou;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Result;
+    }
+}
+
